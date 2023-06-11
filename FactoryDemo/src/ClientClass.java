00454564191deef500b585b3d8693da8f53787ee
AFKstdy <326303052@qq.com>
2023-06-11 13:02:50 +0800

    v1 SimpleFactory pattern

--- FactoryDemo/src/ClientClass.java
+++ FactoryDemo/src/ClientClass.java
@@ -1,7 +1,7 @@
 public class ClientClass{
     public static void main(String[] args) {
         //Factory factory = new BFactory();
-        Factory factory = new AFactory();
+        Factory factory = new BFactory();
         Fruit fruit = factory.CreateFruit();
         fruit.eat();
     }
