68d037642fc5dc52811f1316a9e4b1cff73947eb
AFKstdy <326303052@qq.com>
2023-06-11 11:08:58 +0800

    v1 SimpleFactory pattern

--- FactoryDemo/src/ClientClass.java
+++ FactoryDemo/src/ClientClass.java
@@ -1,14 +1,12 @@
 public class ClientClass{
     public static void main(String[] args) {
-        Factory factory = new Factory();
-        Fruit fruit = factory.CreateFruit("A");
+        //Factory factory = new BFactory();
+        Factory factory = new AFactory();
+        Fruit fruit = factory.CreateFruit();
         fruit.eat();
     }
     public String factory(String fruitname){
-        if(fruitname.equals("Apple"))
-            return "Apple";
-        if(fruitname.equals("Banana"))
-            return "Banana";
+
         return null;
     }
 }
--- FactoryDemo/src/Factory.java
+++ FactoryDemo/src/Factory.java
@@ -1,10 +1,10 @@
 public class Factory{
-    public Fruit CreateFruit(String Kind){
+    public Fruit CreateFruit(){
         //构造了一个水果工厂
-        if(Kind.equals("A"))
-            return new Apple();
-        if(Kind.equals("B"))
-            return new Banana();
+        //if(Kind.equals("A"))
+          //  return new Apple();
+        //if(Kind.equals("B"))
+          //  return new Banana();
         return null;
     }
 }
